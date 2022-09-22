dd88f486b7b1638f1d2060c4fcd7aae7c57c1fb8
suzanneazzam <suzanne.azzam@gmail.com>
2022-09-21 17:36:33 -0700

    completed assignment

---
+++ arrow-function.js
@@ -0,0 +1,14 @@
+const double = (arr) => arr.map((val) => val * 2);
+
+const squareAndFindEvens = (numbers) =>
+  numbers.map((num) => num ** 2).filter((square) => square % 2 === 0);
+
+// function squareAndFindEvens(numbers) {
+//   var squares = numbers.map(function (num) {
+//     return num ** 2;
+//   });
+//   var evens = squares.filter(function (square) {
+//     return square % 2 === 0;
+//   });
+//   return evens;
+// }
